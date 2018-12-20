ee66ae71c9e709846e912510d90aa74acf84a9e2
Eric Brahmann <37987769+EED85@users.noreply.github.com>
2018-12-21 00:54:52 +0100

    new examples

--- Optimization/first_try.py
+++ Optimization/first_try.py
@@ -10,10 +10,17 @@ s = minimize(y,1)
 print('Minimum of y=x**2 is ' + str(round(s.x[0],2)))
 
 
-bnds = (1, None)
 
-s = minimize(fun, (2, 0), method='SLSQP', bounds=bnds)
+#easy constrained optimization
+#min y=x**2 with x>=1
 
+def y(x):
+    return x[0]**2
+
+x0 = np.array([4])
+c = ({'type': 'ineq', 'fun': lambda x:  x[0] - 1}) #x>=1
+s = minimize(y, x0,  constraints=c,method='SLSQP')
+print('Minimum of y=x**2 with respect to x >=1 is ' + str(round(s.x[0],2)))
 
 # solving y = 2*b*x with least sqaures
 #https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
---
+++ Optimization/fitting.py
@@ -0,0 +1,33 @@
+# solving y = 2*b*x with least sqaures
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.least_squares.html
+import numpy as np
+from scipy.optimize import least_squares
+
+y_  = np.array([-1,0.1,1]) #observed data
+x   = np.linspace(-1,1,3) #x values
+#objective function, returning residual, b will be optimized
+def fun(b,x,y_):
+    return 2*b*x-y_
+
+b0 = 0 #start point
+
+s = least_squares(fun,b0, args=(x, y_))
+print('solution of y=2*b*x ' + str(s.x[0]))
+
+
+# solving y = b[0] + 2*b[1]*x = a+b*2*x with least sqaures
+
+
+
+y_  = np.array([-2,-1.1,0]) #observed data
+
+#objective function, returning residual, b will be optimized
+def fun2(b,x,y_):
+    return b[0] + 2*b[1]*x-y_
+
+b0 = np.array([0,0]) #start point
+
+s = least_squares(fun2,b0, args=(x, y_))
+
+
+print('solution 0f y=2*b ' + str(s.x[0]))
