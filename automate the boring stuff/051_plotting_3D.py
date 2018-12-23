0d17b37262dcde1e9d4649375c6be61ec0576e3a
Eric Brahmann <37987769+EED85@users.noreply.github.com>
2018-12-23 07:25:25 +0100

    moving vs files to vs repo / plotting 3d working, but colormap not nice

--- automate the boring stuff/051_plotting_3D.py
+++ automate the boring stuff/051_plotting_3D.py
@@ -1,6 +1,7 @@
 # https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm #Color Map
 import numpy as np
 #Surface PLots
 
@@ -9,8 +10,6 @@ import numpy as np
 def poly_3D(x,y):
     return x**2+y**2
 
-def poly_3D2(M):
-    return M[0]**2+M[1]**2
 
 
     #prepare data
@@ -22,9 +21,11 @@ Y=X
 X,Y = np.meshgrid(X,Y)
 Z = poly_3D(X,Y)
 
+
     #plot
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, Z)
+surf = ax.plot_surface(X, Y, Z,cmap=cm.coolwarm)
+fig.colorbar(surf, shrink=0.1, aspect=1)
 plt.show()
 
