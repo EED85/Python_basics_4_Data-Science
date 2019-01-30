96002f352f9f7340359af696a32b061413339ddc
Eric Brahmann <37987769+EED85@users.noreply.github.com>
2019-01-30 20:55:12 +0100

    added some new packages

--- 01_packages.py
+++ 01_packages.py
@@ -1,17 +1,29 @@
+# MATHEMATICS
 import numpy as np #Mathematics
 import random
-import pandas as np #ETL
+import math # mathematical functions defined by the C standard
+
+# ETL
+
+import pandas as np
+import openpyxl as xl #Reading and Editing Excel
+
+# Plotting
+
 import matplotlib as plt #Use Matlab Code for plotting
 import seaborn as sns #nice plots
 
+# TIME
+
+import time
+from datetime import datetime
+
+# SYSTEM
 import copy #copies lists and dictonaries
-
-import openpyxl as xl #Reading and Editing Excel
-
-
 import os #operating system -> get folder information , set wd etc.
 import sys  #retreiving system information
 import paperclip #get and set clipboard
+import pkg_resources #listung all python packages and its versions
 
 
 #--------------------------------------------------------
---
+++ automate the boring stuff/120_performance.py
@@ -0,0 +1,5 @@
+import time
+t0 = time.clock()
+time.sleep(10)
+t1 = time.clock()
+print('about ' + str(round(t1-t0)) + ' seconds left') #how to format ?
